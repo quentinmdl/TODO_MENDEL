07f3d214a829b319b1152f1f8bf264d74704afbd
Nicolas Faessel <nicolas.faessel@ynov.com>
2020-11-20 10:28:04 +0100

    Add authentification with LiveWire

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,10 +6,17 @@ use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Laravel\Fortify\TwoFactorAuthenticatable;
+use Laravel\Jetstream\HasProfilePhoto;
+use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
 {
-    use HasFactory, Notifiable;
+    use HasApiTokens;
+    use HasFactory;
+    //use HasProfilePhoto;
+    use Notifiable;
+    use TwoFactorAuthenticatable;
 
     /* Relations Eloquent */
 
@@ -83,7 +90,7 @@ class User extends Authenticatable
     }
 
 
-    /**
+     /**
      * The attributes that are mass assignable.
      *
      * @var array
@@ -102,6 +109,8 @@ class User extends Authenticatable
     protected $hidden = [
         'password',
         'remember_token',
+        'two_factor_recovery_codes',
+        'two_factor_secret',
     ];
 
     /**
@@ -111,5 +120,14 @@ class User extends Authenticatable
      */
     protected $casts = [
         'email_verified_at' => 'datetime',
+    ];
+
+    /**
+     * The accessors to append to the model's array form.
+     *
+     * @var array
+     */
+    protected $appends = [
+        //'profile_photo_url',
     ];
 }
